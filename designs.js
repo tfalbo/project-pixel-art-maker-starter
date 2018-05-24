bd29c0758454a09371a154c83c9f17eda9eedbb2
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-05-24 00:55:37 -0300

    starting

--- designs.js
+++ designs.js
@@ -1,10 +1,22 @@
 // Select color input
+var color = $("#inputColorPicker").val();
+
 // Select size input
+var height = $("#inputHeight").val();
+var width = $("#inputWidth").val();;
 
 // When size is submitted by the user, call makeGrid()
+$("#sizePicker").on('submit', makeGrid());
 
 function makeGrid() {
-
 // Your code goes here!
-
+    var content = '';
+    for (i=0; i < height; i++){
+        content += '<tr>';
+        for(j=0; j < width;j++){
+            content += '<td class=".cell"></td>';
+        }
+        content+= '</tr>';
+    }
+    $('#pixelCanvas').append(content);
 }
