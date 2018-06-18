d540164cb9da0a206472094ac13a803c28a00cd1
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-06-18 11:26:27 -0300

    try to fix JS

--- designs.js
+++ designs.js
@@ -1,30 +1,32 @@
-// Select color input
-const color = $("#inputColorPicker").val();
+const color_picker = $('#colorPicker');
 
 // Select size input
-const row_input = $("#inputHeight");
-const column_input = $("#inputWidth");
+const row_input = $('#inputHeight') ;
+const column_input = $('#inputWidth');
 
 // When size is submitted by the user, call makeGrid()
-$("#sizePicker").on('submit', function(event){
+$('#sizePicker').on('submit', function(event){
     event.preventDefault();
     makeGrid();
 });
 
 
 // Coloring listener
-$(".cell").on('click', function(){
+$('.cell').on('click', function(){
+    let color = color_picker.val();
     $(this).css('background-color', color);
 });
 
+
 function makeGrid() {
-// Your code goes here!
     let row_count = row_input.val();
     let column_count = column_input.val();
 
+    // $('#pixelCanvas').innerHTML = '';
+
     var content = '';
 
-    for (i=0; i < row_count; i++){
+    for (let i=0; i < row_count; i++){
         content += '<tr>';
         for(j=0; j < column_count ;j++){
             content += '<td class="cell"></td>';
